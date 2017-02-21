c545c2be9027593d93f41d296a647b96f545ff22
pangzheng <pangzheng@homtail.com>
2017-02-21 14:38:19 +0800

    add harbor service listen ip

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
+. ./config.cfg
 base_dir=$(cd `dirname $0` && pwd)
 cd $base_dir
 #./loadimage.sh
 ./images/load.sh
 ./build_config.sh
+if [ -f docker-compose.yml ];then
+        rm -rf docker-compose.yml
+fi
+cp docker-compose.yml.templ docker-compose.yml && \
+sed -i 's/--harbor_ip--/'$HARBOR_HARBOR_IP'/g'  docker-compose.yml && \
 docker-compose -p dataman up -d
 #./import_sql.sh

163a5b2fa6cb9aa43e28a4cd935864ae70a877a3
pangzheng <pangzheng@homtail.com>
2017-02-23 11:48:59 +0800

    add harbor images change

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ cd $base_dir
 if [ -f docker-compose.yml ];then
         rm -rf docker-compose.yml
 fi
-cp docker-compose.yml.templ docker-compose.yml && \
+cp docker-compose.yml.conf docker-compose.yml && \
 sed -i 's/--harbor_ip--/'$HARBOR_HARBOR_IP'/g'  docker-compose.yml && \
 docker-compose -p dataman up -d
 #./import_sql.sh

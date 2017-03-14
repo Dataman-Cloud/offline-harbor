870aa853b77b72a39d61b465a5fba65eebdd99d0
刘金烨 <75710541@qq.com>
2017-03-14 10:08:48 +0800

    down.sh add delete dead container

--- down.sh
+++ down.sh
@@ -3,3 +3,7 @@ base_dir=$(cd `dirname $0` && pwd)
 cd $base_dir
 
 docker-compose -p dataman down
+deadids=`docker ps -aq --filter "STATUS=dead"`
+if [ ! -z "$deadids" ];then
+        docker rm -f $deadids
+fi

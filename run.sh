d4c25a38ef807caa94765a2d99d37b929152be78
pangzheng <pangzheng@hotmail.com>
2017-03-21 12:01:17 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -11,13 +11,6 @@ cd $base_dir
 # build config
 ./build_config.sh
 
-# setting compose
-if [ -f docker-compose.yml ];then
-        rm -rf docker-compose.yml
-fi
-cp docker-compose.yml.conf docker-compose.yml && \
-sed -i 's/--harbor_ip--/'$HARBOR_HARBOR_IP'/g'  docker-compose.yml && \
-
 # run server    
 export COMPOSE_HTTP_TIMEOUT=300
 docker-compose -p dataman up -d
